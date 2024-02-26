2c702ece9e24331cb1bb95f2957ef192a612a084
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2024-02-26 12:36:41 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -13,44 +13,37 @@ int _printf(const char *format, ...)
 	char *begin = NULL;
 	va_list args;
 	fmt_opt_t opt = OPT_INIT;
-	
+
 	va_start(args, format);
 
-    if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_opt(&opt, args);
 
-    for (p = (char *)format; *p; p++)
-    {
-	    init_opt(&opt, args);
+		if (*p != '%')
+		{
+			count += c_putchar(*p);
+			continue;
+		}
+		p++;
 
-        
-	    if (*p != '%')
-        
-	    {
-            
-		    count += c_putchar(*p);
-            
-		    continue;
-        
-	    }
+		while (get_f(p, &opt))
+			p++;
+		p = get_w(p, &opt, args);
+		p = get_preci(p, &opt, args);
 
-        p++;
-        while (get_f(p, &opt))
-            p++;
-
-        p = get_w(p, &opt, args);
-        p = get_preci(p, &opt, args);
-
-        if (get_m(p, &opt))
-            p++;
-	if (!get_speci(p))
-            count += _printing_range(begin, p, opt.L || opt.H ? p - 1 : (char *)0);
-	else
-            count += get_print_func(p, args, &opt);
-    }
-    c_putchar(BUFFER_FLUSH);
-    va_end(args);
-    return (count);
+		if (get_m(p, &opt))
+			p++;
+		if (!get_speci(p))
+			count += _printing_range(begin, p, opt.L || opt.H ? p - 1 : (char *)0);
+		else
+			count += get_print_func(p, args, &opt);
+	}
+	c_putchar(BUFFER_FLUSH);
+	va_end(args);
+	return (count);
 }
